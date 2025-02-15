49adec422703273808abe6c9d74201fc4a2d34c6
Richa <gargricha1209@gmail.com>
2025-02-15 17:25:34 +0530

    pose detection

--- flex-it-out/src/App.jsx
+++ flex-it-out/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import Home from "./Pages/HomePage";
 import SignIn from "./Components/Signin";
 import SignUp from "./Components/Signup";
+import PoseDetection from "./Pages/PoseDetection"; 
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/pose-detection" element={<PoseDetection />} /> {/* ✅ New route */}
       </Routes>
     </>
   );
